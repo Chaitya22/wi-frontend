cd480844a516feb90859942354885b59888b0f82
Chaitya Shah <chaityashah22@gmail.com>
2021-07-27 13:41:41 +0530

    Navbar seperated

---
+++ src/component/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import HelpIcon from '@material-ui/icons/Help';
+import PersonIcon from '@material-ui/icons/Person';
+
+function Navbar() {
+	const [ value, setValue ] = React.useState(0);
+	return (
+		<div className=' card fixed-bottom '>
+			<BottomNavigation
+				showLabels
+				value={value}
+				onChange={(event, newValue) => {
+					setValue(newValue);
+				}}
+			>
+				<BottomNavigationAction label='Home' icon={<BusinessCenterIcon />} />
+				<BottomNavigationAction label='Recharge' icon={<AccountBalanceWalletIcon />} />
+				<BottomNavigationAction label='Post Job' icon={<AddCircleOutlineIcon />} />
+				<BottomNavigationAction label='Help' icon={<HelpIcon />} />
+				<BottomNavigationAction label='Profile' icon={<PersonIcon />} />
+			</BottomNavigation>
+		</div>
+	);
+}
+
+export default Navbar;
